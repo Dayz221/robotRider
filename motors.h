0876e6a8d5a324fbef1a9c747bf3e8b1d844fc03
Dayz221 <rutuchenal37@gmail.com>
2024-04-03 19:12:45 +0300

    test

--- motors.h
+++ motors.h
@@ -11,5 +11,12 @@ public:
     
     void setSpeed(int speed);
     void stopMotor(int break_time);
+    void moveMilliseconds();
 };
 
+class AccMotor : public Motor {
+private:
+    int& accVal;
+public:
+    AccMotor(int dir, int pwm, float k = 1.0f, int& accVar);
+};
