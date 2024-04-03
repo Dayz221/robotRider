4e91d986a25270c56c6f36130b513bd0eeaaf248
Flynn <mavmalyshev@yandex.ru>
2024-04-03 19:54:33 +0300

    Servo0.0.2

--- GripperSystem.cpp
+++ GripperSystem.cpp
@@ -1,55 +1,47 @@
 #include "GripperSystem.h"
 
-class GripperServo{
-public:
-    GripperServo(int pin, int close_val, int open_val) {
-        this->pin = pin;
-        this->close_val = close_val;
-        this->open_val = open_val;
-    }
+GripperServo::GripperServo(int pin, int close_val, int open_val) {
+    this->pin = pin;
+    this->close_val = close_val;
+    this->open_val = open_val;
+}
 
-    GripperServo() : pin(0), close_val(0), open_val(0) {}
+GripperServo::GripperServo() : pin(0), close_val(0), open_val(0) {}
     
-    GripperServo operator= (GripperServo& other) {
-        this->pin = other.pin;
-        this->close_val = other.close_val;
-        this->open_val = other.open_val;
-    }
+GripperServo GripperServo::operator= (GripperServo& other) {
+    this->pin = other.pin;
+    this->close_val = other.close_val;
+    this->open_val = other.open_val;
+    return *this;
+}
 
-    void open() {
+void GripperServo::open() {
 
-    }
-    void close() {
+}
 
-    }
+void GripperServo::close() {
 
-private:
-    int pin;
-    int close_val, open_val;
-};
+}
 
-class GripperSystem{
-public:
-    GripperSystem(GripperServo& gripper, GripperServo& lifter) {
-        this->gripper = gripper;
-        this->lifter = lifter;
-    }
 
-    void open_gripper()  {
-        gripper.open();
-    }
 
-    void close_gripper() {
-        gripper.close();
-    }
+GripperSystem::GripperSystem(GripperServo& gripper, GripperServo& lifter) {
+    this->gripper = gripper;
+    this->lifter = lifter;
+}
 
-    void open_lifter()   {
-        lifter.open();
-    }
+void GripperSystem::open_gripper()  {
+    gripper.open();
+}
 
-    void close_lifter()  {
-        lifter.close();
-    }
-private:
-    GripperServo gripper, lifter;
-};
+void GripperSystem::close_gripper() {
+    gripper.close();
+}
+
+void GripperSystem::open_lifter()   {
+    lifter.open();
+}
+
+void GripperSystem::close_lifter()  {
+    lifter.close();
+}
--- GripperSystem.h
+++ GripperSystem.h
@@ -2,12 +2,12 @@
 
 class GripperServo{
 public:
-    GripperServo(int pin, int close_val, int open_val) {};
-    GripperServo() : pin(0), close_val(0), open_val(0) {};
-    GripperServo operator= (GripperServo& other) {};
+    GripperServo(int pin, int close_val, int open_val);
+    GripperServo();
+    GripperServo operator= (GripperServo& other);
 
-    void open() {};
-    void close() {};
+    void open();
+    void close();
 
 private:
     int pin;
@@ -16,11 +16,11 @@ private:
 
 class GripperSystem{
 public:
-    GripperSystem(GripperServo& gripper, GripperServo& lifter) {};
-    void open_gripper()  {};
-    void close_gripper() {};
-    void open_lifter()   {};
-    void close_lifter()  {};
+    GripperSystem(GripperServo& gripper, GripperServo& lifter);
+    void open_gripper();
+    void close_gripper();
+    void open_lifter();
+    void close_lifter();
 private:
     GripperServo gripper, lifter;
 };
